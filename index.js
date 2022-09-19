c7415c11a95c6a1c323aa026caa03eaa4180ac91
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2022-09-18 23:29:03 -0500

    modificacion del archivo para el desafio de DOM

--- index.js
+++ index.js
@@ -38,14 +38,16 @@ const usuarioUno = crearUsuario()
 console.info(usuarioUno)
 
 
-function mostrarMenu() {
-    let opcion = prompt(
-      "Bienvenido, seleccione una opción \n1. Agregar cancion a la lista\n2. Eliminar cancion de la lista\n3. Salir del menu"
-    );
-    return opcion;
-  }
+// function mostrarMenu() {
+//     let opcion = prompt(
+//       "Bienvenido, seleccione una opción \n1. Agregar cancion a la lista\n2. Eliminar cancion de la lista\n3. Salir del menu"
+//     );
+//     return opcion;
+//   }
 
-  mostrarMenu()
+//   mostrarMenu()
+
+
   
   const bestSongs = [
     "Bohemian Rhapsody",
@@ -70,26 +72,81 @@ let otherSongs = [
 
 console.log(bestSongs)
 
-for (let index = 0; index < bestSongs.length; index++) {
-    console.log("index: "+ index + " : "+ bestSongs [index])
-   }
+const tuLista = []
 
-const allSongs = bestSongs.concat (otherSongs)
-console.log (allSongs)
+let opcion
 
-
-
-let elementoEncontrado = bestSongs.find(
-  (elemento) => elemento === "Lonely"
-)
-console.log(elementoEncontrado)
+do {
+    opcion = parseInt(prompt("Bienvenido, seleccione una opción \n1.Best Songs\n2.Crea tu propia lista lista\n3.Ver tu selección \n4 Salir del menu"))
 
 
-let newList = bestSongs.map((elemento) => ({
-elemento,
-}));
+    switch (opcion) {
+        case 1:
 
-console.log(newList)
+            function mostrarMenu() {
+                let mensaje = "Best Songs"
+                let count = 1
+
+                for (let song of bestSongs) {
+                    mensaje += `\n${count}. ${song}`
+                    count++
+                }
+                //  mensaje += `\n${count}. Finalizar`
+                return mensaje
+            }
+
+            alert(mostrarMenu())
+
+            break
+        case 2:
+            let song = prompt("Escribe el nombre de la canción")
+            tuLista.push(song)
+            console.log(tuLista)
+
+            break
+        case 3:
+
+            function nuevaLista() {
+                let mensaje = "Tu selección"
+                let count = 1
+
+                for (let song of tuLista) {
+                    mensaje += `\n${count}. ${song}`
+                    count++
+                }
+                return mensaje
+            }
+            alert(nuevaLista())
+            break
+
+        case 4:
+            alert("Gracias por tu visita")
+
+    }
+
+} while (opcion !== 4)
+
+
+// for (let index = 0; index < bestSongs.length; index++) {
+//     console.log("index: "+ index + " : "+ bestSongs [index])
+//    }
+
+// const allSongs = bestSongs.concat (otherSongs)
+// console.log (allSongs)
+
+
+
+// let elementoEncontrado = bestSongs.find(
+//   (elemento) => elemento === "Lonely"
+// )
+// console.log(elementoEncontrado)
+
+
+// let newList = bestSongs.map((elemento) => ({
+// elemento,
+// }));
+
+// console.log(newList)
 
 // function main(){
 //   crearUsuario()
@@ -97,3 +154,57 @@ console.log(newList)
 // }
 
 // main()
+
+console.log(document)
+
+console.log(document.head)
+console.log(document.body)
+
+
+
+const playList = [
+    {
+        id: 1,
+        name: "We Are The Champions",
+
+    },
+
+    {
+        id: 2,
+        name: "Lonely",
+
+    },
+
+    {
+        id: 3,
+        name: "Bohemian Rhapsody",
+    },
+
+    {
+        id: 4,
+        name: "Show Must Go On",
+    },
+
+    {
+        id: 5,
+        name: "We Will Rock You",
+    },
+
+]
+
+const contenedorCanciones = document.getElementById("contenedor-canciones")
+
+
+for (const cancion of playList) {
+  let column = document.createElement("div");
+  column.className = "col-md-6 mt-4 ";
+  column.id = `columna-${cancion.id}`;
+  column.innerHTML = `
+      <div class="card">
+          <div class="card-body">
+          <p class="card-text"> Name: <b>${cancion.name}</b></p>
+          </div>
+      </div>`
+
+  contenedorCanciones.append(column);
+}
