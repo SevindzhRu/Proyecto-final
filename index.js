8f7f3684196131508ba7f0292873eb46c9419345
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2022-09-25 22:28:06 -0500

    unas modificaciones

--- index.js
+++ index.js
@@ -1,236 +1,203 @@
-// class Usuario {
-//     constructor(nombre, apellido, edad, pais, ciudad, esQueenFan) {
-//         this.nombre = nombre
-//         this.apellido = apellido
-//         this.edad = edad
-//         this.pais = pais
-//         this.ciudad = ciudad
-//         this.esQueenFan = esQueenFan
-//     }
-// }
+let songs = []
+let user
 
-// function crearUsuario() {
-//   let nombre = prompt("Ingresa tu nombre")
-//   let apellido = prompt("Ingresa tu apellido")
-//   let edad = prompt("Ingresa tu edad")
-//   let pais = prompt("Ingresa tu pais")
-//   let ciudad = prompt("Ingresa tu ciudad")
-//   let esQueenFan = prompt("¿Eres fan de Queen? (Si/No)")
+let formularioIdentificacion
+let contenedorIdentificacion
+let contenedorUsuario
+let textoUsuario
+let botonLimpiarStorage
 
-//   if (esQueenFan.toLocaleLowerCase () === "si") {
-//     esQueenFan = true
-//   } else {
-//     alert ("Te recomiendo escucharlos")
-//   }
+let modalAddSong
+let botonAddSong
+let formulario
+let inputId
+let inputName
+let inputYear
+let inputAlbum
+let contenedorCanciones
+let botonesCloseModalAddSong
 
-//   const objetoUsuario = new Usuario(
-//     nombre,
-//     apellido,
-//     edad,
-//     pais,
-//     ciudad,
-//     esQueenFan
-//   );
-//   return objetoUsuario
-// }
+class Song {
+  constructor(id, name, year, album) {
+    this.id = id
+    this.name = name.toUpperCase()
+    this.year = year
+    this.album = album;
+    
+  }
+}
 
-// const usuarioUno = crearUsuario()
-// console.info(usuarioUno)
+function inicializarElementos() {
+  formularioIdentificacion = document.getElementById("formularioIdentificacion")
+  inputUsuario = document.getElementById("inputUsuario")
+  contenedorIdentificacion = document.getElementById("contenedorIdentificacion")
+  contenedorUsuario = document.getElementById("contenedorUsuario")
+  textoUsuario = document.getElementById("textoUsuario")
 
+  botonLimpiarStorage = document.getElementById("cleanStorage")
+  formulario = document.getElementById("formularioAddSong")
+  inputId = document.getElementById("inputId")
+  inputName = document.getElementById("inputNameSong")
+  inputYear = document.getElementById("inputYear")
+  inputAlbum = document.getElementById("inputAlbum")
+  contenedorCanciones = document.getElementById("contenedorCanciones")
 
-// function mostrarMenu() {
-//     let opcion = prompt(
-//       "Bienvenido, seleccione una opción \n1. Agregar cancion a la lista\n2. Eliminar cancion de la lista\n3. Salir del menu"
-//     );
-//     return opcion;
-//   }
-
-//   mostrarMenu()
-
-
-  
-  const bestSongs = [
-    "Bohemian Rhapsody",
-    "We Are The Champions",
-    "We Will Rock You",
-    "I Want To Break Free",
-    "Radio Ga Ga",
-    "Lonely",
-    "The Show Must Go On",
-    "Killer Queen",
-    "Somebody To Love",
-    "Dont Stop Me Now"
-]
-
-// let otherSongs = [
-//     "Another One Bites The Dust",
-//     "Love Of My Life",
-//     "A King Of Magic",
-//     "Under Pressure",
-//     "Crazy Little Thing Colled Love"
-// ]
-
-// console.log(bestSongs)
-
-// const tuLista = []
-
-// let opcion
-
-// do {
-//     opcion = parseInt(prompt("Bienvenido, seleccione una opción \n1.Best Songs\n2.Crea tu propia lista lista\n3.Ver tu selección \n4 Salir del menu"))
+  botonesCloseModalAddSong = document.getElementsByClassName("btnCloseModalAddSong")
+  modalAddSong = document.getElementById("modalAddSong")
+  botonAddSong = document.getElementById("AddSong")
+  modal = new bootstrap.Modal(modalAddSong)
+}
 
 
-//     switch (opcion) {
-//         case 1:
+function inicializarEventos() {
+  formulario.onsubmit = (event) => validarFormulario(event)
+  formularioIdentificacion.onsubmit = (event) => identificarUsuario(event)
+  botonLimpiarStorage.onclick = eliminarStorage
+  botonAddSong.onclick = openModalAddSong
 
-//             function mostrarMenu() {
-//                 let mensaje = "Best Songs"
-//                 let count = 1
+  for (const boton of botonesCloseModalAddSong) {
+    boton.onclick = closeModalAddSong
+  }
+}
 
-//                 for (let song of bestSongs) {
-//                     mensaje += `\n${count}. ${song}`
-//                     count++
-//                 }
-//                 //  mensaje += `\n${count}. Finalizar`
-//                 return mensaje
-//             }
+function openModalAddSong() {
+  if (user) {
+    modal.show();
+  } else {
+    alert("Enter for add song");
+  }
+}
 
-//             alert(mostrarMenu())
-
-//             break
-//         case 2:
-//             let song = prompt("Escribe el nombre de la canción")
-//             tuLista.push(song)
-//             console.log(tuLista)
-
-//             break
-//         case 3:
-
-//             function nuevaLista() {
-//                 let mensaje = "Tu selección"
-//                 let count = 1
-
-//                 for (let song of tuLista) {
-//                     mensaje += `\n${count}. ${song}`
-//                     count++
-//                 }
-//                 return mensaje
-//             }
-//             alert(nuevaLista())
-//             break
-
-//         case 4:
-//             alert("Gracias por tu visita")
-
-//     }
-
-// } while (opcion !== 4)
+function closeModalAddSong() {
+  formulario.reset()
+  modal.hide()
+}
 
 
-// for (let index = 0; index < bestSongs.length; index++) {
-//     console.log("index: "+ index + " : "+ bestSongs [index])
-//    }
+function eliminarStorage() {
+  localStorage.clear()
+  user = ""
+  songs = []
+  mostrarFormularioIdentificacion()
+  pintarSongs()
+}
 
-// const allSongs = bestSongs.concat (otherSongs)
-// console.log (allSongs)
+function identificarUsuario(event) {
+  event.preventDefault()
+  user = inputUsuario.value
+  formularioIdentificacion.reset()
+  actualizarUsuarioStorage()
+  mostrarTextoUsuario()
+}
 
+function mostrarTextoUsuario() {
+  contenedorIdentificacion.hidden = true
+  contenedorUsuario.hidden = false
+  textoUsuario.innerHTML += ` ${user}`
+}
 
+function mostrarFormularioIdentificacion() {
+  contenedorIdentificacion.hidden = false
+  contenedorUsuario.hidden = true
+  textoUsuario.innerHTML = ``
+}
 
-// let elementoEncontrado = bestSongs.find(
-//   (elemento) => elemento === "Lonely"
-// )
-// console.log(elementoEncontrado)
+function validarFormulario(event) {
+  event.preventDefault()
+  let idSong = inputId.value
+  let name = inputName.value
+  let year = parseFloat(inputYear.value)
+  let album = inputAlbum.value
 
+  const idExiste = songs.some((song) => song.id === idSong)
+  if (!idExiste) {
+    let song = new Song(idSong, name, year, album,)
 
-// let newList = bestSongs.map((elemento) => ({
-// elemento,
-// }));
+    songs.push(song)
+    formulario.reset()
+    alert("You have added a song")
+    actualizarSongsStorage()
+    pintarSongs()
+  } else {
+    alert("This id already exists")
+  }
+}
 
-// console.log(newList)
+function deleteSongs(idSong) {
+  let columnDelete = document.getElementById(`column-${idSong}`)
+  let indexDelete = songs.findIndex(
+    (song) => Number(song.id) === Number(idSong)
+  )
 
-// function main(){
-//   crearUsuario()
-//   mostrarMenu()
-// }
+  songs.splice(indexDelete, 1);
+  columnDelete.remove();
+  actualizarSongsStorage();
+}
 
-// main()
+function pintarSongs() {
+  contenedorCanciones.innerHTML = "";
+  songs.forEach((song) => {
+    let column = document.createElement("div");
+    column.className = "col-md-4 mt-3";
+    column.id = `columna-${song.id}`;
+    column.innerHTML = `
+            <div class="card">
+                <div class="card-body">
+                <p class="card-text">ID:
+                    <b>${song.id}</b>
+                </p>
+                <p class="card-text">Name:
+                    <b>${song.name}</b>
+                </p>
+                <p class="card-text">Year:
+                    <b>${song.year}</b>
+                </p>
+                <p class="card-text">Album:
+                    <b>${song.album}</b>
+                </p>
+                </div>
+                <div class="card-footer">
+                  <button class="btn btn-danger" id="botonDelete-${song.id}">Delete</button>
+                </div>
+            </div>`
 
+    contenedorCanciones.append(column)
 
-// ============ DOM ======================
+    let botonDelete = document.getElementById(`botonDelete-${song.id}`)
+    botonDelete.onclick = () => deleteSongs(song.id)
+  })
+}
 
-// console.log(document)
+function actualizarSongsStorage() {
+  let songsJSON = JSON.stringify(songs)
+  localStorage.setItem("songs", songsJSON);
+}
 
-// console.log(document.head)
-// console.log(document.body)
+function actualizarUsuarioStorage() {
+  localStorage.setItem("user", user)
+}
 
+function getSongStorage() {
+  let songsJSON = localStorage.getItem("songs")
+  if (songsJSON) {
+    songs = JSON.parse(songsJSON)
+    pintarSongs()
+  }
+}
 
+function getUserStorage() {
+  let usuarioAlmacenado = localStorage.getItem("user")
+  if (usuarioAlmacenado) {
+    user = usuarioAlmacenado
+    mostrarTextoUsuario()
+  }
+}
 
-// const playList = [
-//     {
-//         id: 1,
-//         name: "We Are The Champions",
+function main() {
+  inicializarElementos()
+  inicializarEventos()
+  getSongStorage()
+  getUserStorage()
+}
 
-//     },
-
-//     {
-//         id: 2,
-//         name: "Lonely",
-
-//     },
-
-//     {
-//         id: 3,
-//         name: "Bohemian Rhapsody",
-//     },
-
-//     {
-//         id: 4,
-//         name: "Show Must Go On",
-//     },
-
-//     {
-//         id: 5,
-//         name: "We Will Rock You",
-//     },
-
-// ]
-
-// const contenedorCanciones = document.getElementById("contenedor-canciones")
-
-
-// for (const cancion of playList) {
-//   let column = document.createElement("div");
-//   column.className = "col-md-6 mt-4 ";
-//   column.id = `columna-${cancion.id}`;
-//   column.innerHTML = `
-//       <div class="card">
-//           <div class="card-body">
-//           <p class="card-text"> Name: <b>${cancion.name}</b></p>
-//           </div>
-//       </div>`
-
-//   contenedorCanciones.append(column);
-// }
-
-// EVENTOS
-
-const mixList = document.querySelector(".mix");
-const button = document.querySelector(".show-list");
-const total = document.querySelector(".total");
-
-button.addEventListener("click", function () {
-  mixInfo(bestSongs);
-  mixList.classList.remove("hide");
-  button.classList.add("hide");
-});
-
-total.innerText = `${bestSongs.length} great songs!`;
-
-const mixInfo = function (mix) {
-  mix.forEach(function (song, index) {
-    let li = document.createElement("li");
-    li.classList.add("song");
-    li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`;
-    mixList.append(li);
-  });
-};
+main()
