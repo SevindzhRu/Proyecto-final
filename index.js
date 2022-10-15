fa944f2ce69bc20b4d7775c0171c670513f1589f
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2022-10-14 21:50:31 -0500

    modificando videos

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ function pintarSongs() {
                 </p>
                 <p class="card-text">watch:
                 <b>${song.watch}</b>
-                <iframe widht="560" height="315" src="https://wwww.youtube.com/embed/${videoId}"
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}"
                 title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
             </p>
